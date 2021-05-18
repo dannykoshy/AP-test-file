4b73009b4f7089b571684d3913a48b8d6c3ca02d
Danny Koshy <dannykoshy@gmail.com>
2021-05-18 12:46:56 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -1,12 +1,11 @@
-function validationFuntion(a) {
+function validationFuntion() {
     console.log('validation funtion is executed');
-    console.log(a);
     return true;
 }
 
 function onClickFuntion(param) {
     console.log('Function executed');
-    console.log('param', param);
+    console.log(param);
 }
 
 export default {

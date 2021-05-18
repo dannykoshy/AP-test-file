36ebad2dee94bebb4aa241d76859676a85fd590e
Danny Koshy <dannykoshy@gmail.com>
2021-05-18 21:06:18 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -4,7 +4,7 @@ function validateViewConversationThread(params) {
 }
 
 function onClickViewConversationThread(params) {
-    console.log('onClick ', param);
+    console.log('onClick ', params);
     window.open('https://web.yammer.com/main/users/eyJfdHlwZSI6IlVzZXIiLCJpZCI6Ijc3MjcyMjI3ODQwIn0');
 }
 

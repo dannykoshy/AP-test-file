ab743fbf1569ff0bcb2acb0f2fa8df3ec03a04a7
Danny Koshy <dannykoshy@gmail.com>
2021-05-19 13:56:56 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -6,6 +6,14 @@ function validateViewConversationThread(params) {
     return true;
 }
 
+function validateViewConversations(params) {
+    console.log('validation ', params);
+    if(params.metadata.processDefinationName !== "Yammer Notification") {
+        return false;
+    }
+    return true;
+}
+
 function onClickViewConversationThread(params) {
     console.log('onClick ', params);
     
@@ -23,7 +31,26 @@ function onClickViewConversationThread(params) {
 	})
 }
 
+function onClickViewConversations(params) {
+    console.log('onClick ', params);
+    
+    const url = "/Extension/FetchUsingEncodedData";
+    const query = "SELECT [MESSAGE_THREAD_ID] as threadid FROM [WF_SN_MSG_THREADS] where [ACTIVITY_INST_ID]= '" + params.metadata.ActivityInstID + "'" + "and SOCIAL_NETWORK_NAME = 'yammer'";
+    const data = {
+	encodedData: btoa(query)
+    };
+    const response = params.serviceClient.postJSON(url, data);
+	response.then((response) => {
+	  console.log(response);
+	  //window.open('https://web.yammer.com/main/users/eyJfdHlwZSI6IlVzZXIiLCJpZCI6Ijc3MjcyMjI3ODQwIn0');
+	}, (error) => {
+	  console.log(error);
+	})
+}
+
 export default {
+    validateViewConversations,
     validateViewConversationThread,
+    onClickViewConversations,
     onClickViewConversationThread
 };

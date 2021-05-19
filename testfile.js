5c9f2fab26d2285d7d96cfb31b6ace5eb51904ad
Danny Koshy <dannykoshy@gmail.com>
2021-05-19 14:05:22 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -14,6 +14,14 @@ function validateViewConversations(params) {
     return true;
 }
 
+function validateMSTeamActivities(params) {
+    console.log('validation ', params);
+    if(params.metadata.processDefinationName !== "MS Teams Activities") {
+        return false;
+    }
+    return true;
+}
+
 function onClickViewConversationThread(params) {
     console.log('onClick ', params);
     
@@ -48,9 +56,28 @@ function onClickViewConversations(params) {
 	})
 }
 
+function onClickMSTeamActivities(params) {
+    console.log('onClick ', params);
+    
+    const url = "/Extension/FetchUsingEncodedData";
+    const query = "SELECT [MESSAGE_THREAD_ID] as threadid FROM [WF_SN_MSG_THREADS] where [ACTIVITY_INST_ID]= '" + params.metadata.ActivityInstID + "'" + "and SOCIAL_NETWORK_NAME = 'yammer'";
+    const data = {
+	encodedData: btoa(query)
+    };
+    const response = params.serviceClient.postJSON(url, data);
+	response.then((response) => {
+	  console.log(response);
+	  //window.open('https://web.yammer.com/main/users/eyJfdHlwZSI6IlVzZXIiLCJpZCI6Ijc3MjcyMjI3ODQwIn0');
+	}, (error) => {
+	  console.log(error);
+	})
+}
+
 export default {
     validateViewConversations,
     validateViewConversationThread,
+    validateMSTeamActivities,
     onClickViewConversations,
-    onClickViewConversationThread
+    onClickViewConversationThread,
+    onClickMSTeamActivities	
 };

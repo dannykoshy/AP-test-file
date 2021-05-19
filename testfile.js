5069f0e7fc44794d5aebca8ad0bc8f5ea67c5fee
Danny Koshy <dannykoshy@gmail.com>
2021-05-19 12:33:10 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -8,6 +8,12 @@ function validateViewConversationThread(params) {
 
 function onClickViewConversationThread(params) {
     console.log('onClick ', params);
+    
+    var query = "SELECT [MESSAGE_THREAD_ID] as threadid FROM [WF_SN_MSG_THREADS] where [ACTIVITY_INST_ID]= '" + params.metadata.ActivityInstID + "'" + "and SOCIAL_NETWORK_NAME = 'yammer'";
+	var JSONObject = {
+		encodedData: btoa(query)
+	};
+    console.log(JSONObject);
     window.open('https://web.yammer.com/main/users/eyJfdHlwZSI6IlVzZXIiLCJpZCI6Ijc3MjcyMjI3ODQwIn0');
 }
 

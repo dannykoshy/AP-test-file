474a8d78c97aae75d5cf56c10561303a3a239fca
Danny Koshy <dannykoshy@gmail.com>
2021-05-20 14:19:00 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -31,20 +31,20 @@ function onClickViewConversationThread(params) {
 }
 
 function onClickViewConversations(params) {
-    console.log('onClick ', params);
-
     const url = "/Extension/FetchUsingEncodedData";
-    const query = "SELECT [MESSAGE_THREAD_ID] as threadid FROM [WF_SN_MSG_THREADS] where [ACTIVITY_INST_ID]= '" + params.metadata.ActivityInstID + "'" + "and SOCIAL_NETWORK_NAME = 'yammer'";
+    const query = "SELECT [MESSAGE_THREAD_ID] as threadid FROM [WF_SN_MSG_THREADS] where [ACTIVITY_INST_ID]= '" + params.metadata.activityInstID + "'" + "and SOCIAL_NETWORK_NAME = 'yammer'";
     const data = {
         encodedData: btoa(query)
     };
     const response = params.serviceClient.postJSON(url, data);
     response.then((response) => {
-        console.log(response);
-        //window.open('https://web.yammer.com/main/users/eyJfdHlwZSI6IlVzZXIiLCJpZCI6Ijc3MjcyMjI3ODQwIn0');
-    }, (error) => {
-        console.log(error);
-    })
+        return response.json();
+    }).then((response) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(response, "text/xml");
+        const groupId = xmlDoc.getElementsByTagName("threadid")[0].textContent.split(':')[1];
+        console.log(groupId);
+    });
 }
 
 function onClickMSTeamActivities(params) {
